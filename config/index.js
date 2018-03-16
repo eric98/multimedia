98f7e8a4f2a450dcbf7271c48564119976b34f39
eric98 <ergare.17@gmail.com>
2018-03-16 16:42:21 +0100

    Sync

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ module.exports = {
   build: {
     env: require('./prod.env'),
     index: path.resolve(__dirname, '../dist/index.html'),
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsRoot: path.resolve(__dirname, '../docs'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     productionSourceMap: true,

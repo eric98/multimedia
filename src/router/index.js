f14f045772cd59228e68f9dc14d5861d70395ee9
eric98 <ergare.17@gmail.com>
2018-04-17 17:33:07 +0200

    Sync

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import HelloWorld from '@/components/HelloWorld'
-import MusicComponent from '@/components/MusicComponent'
-import ClockComponent from '@/components/ClockComponent'
-import GameComponent from '@/components/GameComponent'
+// import HelloWorld from '@/components/HelloWorld'
+// import MusicComponent from '@/components/MusicComponent'
+// import ClockComponent from '@/components/ClockComponent'
+// import GameComponent from '@/components/GameComponent'
 
-// const HelloWorld = () => import('@/components/HelloWorld')
-// const MusicComponent = () => import('@/components/MusicComponent')
-// const ClockComponent = () => import('@/components/ClockComponent')
-// const GameComponent = () => import('@/components/GameComponent')
+const HelloWorld = () => import('@/components/HelloWorld')
+const MusicComponent = () => import('@/components/MusicComponent')
+const ClockComponent = () => import('@/components/ClockComponent')
+const GameComponent = () => import('@/components/GameComponent')
 
 Vue.use(Router)
 
